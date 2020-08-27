aadae743123114f5b33388e7eabaf613731eb09a
idan <idan@takescoop.com>
2020-08-27 15:05:51 -0400

    add billing budget and cerebro project

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "billing_account" {
+    type = string
+}
+
+variable "project_id" {
+    type = string
+}
+
+variable "notification_email" {
+    type = string
+}
+
+variable "org_id" {
+    type = string
+}
+
+variable "region" {
+    type = string
+}
